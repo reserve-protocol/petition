e01543df824e9e446dc29711a77b28d1bd4d0e90
Victor Madureri <41347134+vmmq@users.noreply.github.com>
2021-09-09 18:18:08 -0400

    total signers

--- assets/js/pages/petition/index.js
+++ assets/js/pages/petition/index.js
@@ -73,6 +73,7 @@ const countRef = db.collection('count');
 countRef.doc("count")
     .onSnapshot((doc) => {
         document.querySelector('#signers').innerHTML = doc.data()?.count || 0;
+        document.querySelector('#signers_total').innerHTML = (doc.data()?.change_org_count + doc.data()?.count) || 0;
     });
 
 // Listen for form submit
