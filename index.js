72783c7a265a221cfa60df5c590f4a3b38e2f4e9
KevinMoll-ls <35275573+KevinMoll-ls@users.noreply.github.com>
2021-09-09 13:06:30 -0400

    changes

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
+var petitionLink = "https://reserve.org/petition/"
+
+function shareEmail() {
+    window.location.href = "mailto:?subject=Ask the United Nations to recognize the access to a stable currency as a human right.&body="+petitionLink;
+}
+
+$('#copylink').tooltip({trigger: 'click'})
+
+$('#copylink').click(function(){
+    navigator.clipboard.writeText(petitionLink);
+
+    setTimeout(function () {
+		$('#copylink').tooltip('hide')
+	}, 500);
+})
+
+/* FIREBASE */
+
 const config = {
-  apiKey: "AIzaSyAP0TrzOfImB7upXH-LHh_4VcL3BwJeH1Y",
-  authDomain: "petition-project-defac.firebaseapp.com",
-  projectId: "petition-project-defac",
+	apiKey: 'AIzaSyAP0TrzOfImB7upXH-LHh_4VcL3BwJeH1Y',
+	authDomain: 'petition-project-defac.firebaseapp.com',
+	projectId: 'petition-project-defac',
 };
 
 const app = firebase.initializeApp(config);
 const db = app.firestore();
 
 // Reference messages collection
-const messagesRef = db.collection("messages");
-const countRef = db.collection("count");
+const messagesRef = db.collection('messages');
+const countRef = db.collection('count');
 
 // Listen for form submit
 document.getElementById('contactForm').addEventListener('submit', submitForm);
@@ -18,47 +36,50 @@ document.getElementById('contactForm').addEventListener('submit', submitForm);
 
 // Submit form
 function submitForm(e) {
-  e.preventDefault();
+	e.preventDefault();
 
-  //Get value
-  const name = getInputVal('name');
-  const company = getInputVal('company');
-  const email = getInputVal('email');
-  const phone = getInputVal('phone');
-  const message = getInputVal('message');
+	//Get value
+	const name = getInputVal('name');
+    const surname = getInputVal('name');
+	const email = getInputVal('email');
+    const country = getInputVal('country');
+    const city = getInputVal('city');
+	const shareEmail = isChecked('share-email');
 
-  // Save message
-  saveMessage(name, company, email, phone, message);
-
-
-  // Show alert
-  document.querySelector('.alert').style.display = 'block';
-  // // Hide alert after 3 seconds
-  setTimeout(function () {
-    document.querySelector('.alert').style.display = 'none';
-  }, 3000);
-
-  // Clear form
-  document.getElementById('contactForm').reset();
+	// Save message
+	saveMessage(name, surname, email, country, city, shareEmail);
 }
 
 // Function to get form value
 function getInputVal(id) {
-  return document.getElementById(id).value;
+	return document.getElementById(id).value;
+}
+
+function isChecked(id){
+    return document.getElementById(id).checked;
 }
 
 // Save message to firebase
-function saveMessage(name, company, email, phone, message) {
-  messagesRef.add({
-    name: name,
-    company: company,
-    email: email,
-    phone: phone,
-    message: message
-  });
+function saveMessage(name, surname, email, country, city, shareEmail) {
 
-  countRef.doc("count").update({
-    count: firebase.firestore.FieldValue.increment(1)
-  });
+    $('#sign-btn').prop('disabled', true);
 
+    console.table(name, surname, email, country, city, shareEmail);
+
+	/*messagesRef.add({
+		name: name,
+		surname: surname,
+		email: email,
+		country: country,
+		city: city,
+        shareEmail: shareEmail
+	});
+
+	countRef.doc('count').update({
+		count: firebase.firestore.FieldValue.increment(1),
+	});*/
+
+    setTimeout(function () {
+		window.location.href = 'thank_you.html';
+	}, 1500);
 }
