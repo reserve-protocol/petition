ff677e7a69435557cc350f35d95dbe36aaa3b8f9
Tiberiu Lepadatu <tiberiulepadatu14@gmail.com>
2021-09-09 22:59:07 +0300

    Connect to firebase

--- assets/js/pages/petition/index.js
+++ assets/js/pages/petition/index.js
@@ -1,39 +1,39 @@
 var petitionLink = "https://reserve.org/petition/"
 
 function shareEmail() {
-    window.location.href = "mailto:?subject=Ask the United Nations to recognize the access to a stable currency as a human right.&body="+petitionLink;
+    window.location.href = "mailto:?subject=Ask the United Nations to recognize the access to a stable currency as a human right.&body=" + petitionLink;
 }
 
-$('#copylink').tooltip({trigger: 'click'})
+$('#copylink').tooltip({ trigger: 'click' })
 
-$('#copylink').click(function(){
+$('#copylink').click(function () {
     navigator.clipboard.writeText(petitionLink);
 
     setTimeout(function () {
-		$('#copylink').tooltip('hide')
-	}, 500);
+        $('#copylink').tooltip('hide')
+    }, 500);
 })
 
-$('input').on('input change focusout', function(){
+$('input').on('input change focusout', function () {
     let a = $('#name').val();
     let b = $('#surname').val();
     let c = $('#email').val();
     let d = $('#country').val();
     let e = $('#city').val();
 
-    if(a && b && c){
+    if (a && b && c) {
         $('.input-group').addClass('strong');
     } else {
         $('.input-group').removeClass('strong');
     }
 
-    if(d) {
+    if (d) {
         $('#country').addClass('strong');
     } else {
         $('#country').removeClass('strong');
     }
 
-    if(e) {
+    if (e) {
         $('#city').addClass('strong');
     } else {
         $('#city').removeClass('strong');
@@ -45,7 +45,7 @@ $(window).on('scroll resize', function () {
     let a = $('#fixed-sign-btn').offset().top;
     let b = $('#sign-btn').offset().top;
 
-    if(a >= b) {
+    if (a >= b) {
         $('#fixed-sign').addClass('hide');
     } else {
         $('#fixed-sign').removeClass('hide');
@@ -55,9 +55,9 @@ $(window).on('scroll resize', function () {
 /* FIREBASE */
 
 const config = {
-	apiKey: 'AIzaSyAP0TrzOfImB7upXH-LHh_4VcL3BwJeH1Y',
-	authDomain: 'petition-project-defac.firebaseapp.com',
-	projectId: 'petition-project-defac',
+    apiKey: 'AIzaSyAP0TrzOfImB7upXH-LHh_4VcL3BwJeH1Y',
+    authDomain: 'petition-project-defac.firebaseapp.com',
+    projectId: 'petition-project-defac',
 };
 
 const app = firebase.initializeApp(config);
@@ -66,6 +66,10 @@ const db = app.firestore();
 // Reference messages collection
 const messagesRef = db.collection('messages');
 const countRef = db.collection('count');
+countRef.doc("count")
+    .onSnapshot((doc) => {
+        document.querySelector('#signers').innerHTML = doc.data()?.count || 0;
+    });
 
 // Listen for form submit
 document.getElementById('contactForm').addEventListener('submit', submitForm);
@@ -74,50 +78,34 @@ document.getElementById('contactForm').addEventListener('submit', submitForm);
 
 // Submit form
 function submitForm(e) {
-	e.preventDefault();
+    e.preventDefault();
 
-	//Get value
-	const name = getInputVal('name');
-    const surname = getInputVal('name');
-	const email = getInputVal('email');
-    const country = getInputVal('country');
-    const city = getInputVal('city');
-	const shareEmail = isChecked('share-email');
+    let name = $('#name').val();
+    let surname = $('#surname').val();
+    let email = $('#email').val();
+    let country = $('#country').val();
+    let city = $('#city').val();
+    $('#sign-btn').prop('disabled', true);
 
-	// Save message
-	saveMessage(name, surname, email, country, city, shareEmail);
+    messagesRef.add(
+        { name, surname, email, country, city }
+    );
+
+    countRef.doc('count').update({
+        count: firebase.firestore.FieldValue.increment(1),
+    });
+
+    setTimeout(function () {
+        window.location.href = 'thanks/';
+    }, 1500);
+
 }
 
 // Function to get form value
 function getInputVal(id) {
-	return document.getElementById(id).value;
+    return document.getElementById(id).value;
 }
 
-function isChecked(id){
+function isChecked(id) {
     return document.getElementById(id).checked;
-}
-
-// Save message to firebase
-function saveMessage(name, surname, email, country, city, shareEmail) {
-
-    $('#sign-btn').prop('disabled', true);
-
-    console.table(name, surname, email, country, city, shareEmail);
-
-	/*messagesRef.add({
-		name: name,
-		surname: surname,
-		email: email,
-		country: country,
-		city: city,
-        shareEmail: shareEmail
-	});
-
-	countRef.doc('count').update({
-		count: firebase.firestore.FieldValue.increment(1),
-	});*/
-
-    setTimeout(function () {
-		window.location.href = 'thanks/';
-	}, 1500);
 }
