9839d6156facead139e7173186d361006b469a2e
Tiberiu Lepadatu <tiberiulepadatu14@gmail.com>
2021-09-09 14:34:50 +0300

    Change permission level and move to firestore

--- index.js
+++ index.js
@@ -2,40 +2,37 @@ const config = {
   apiKey: "AIzaSyAP0TrzOfImB7upXH-LHh_4VcL3BwJeH1Y",
   authDomain: "petition-project-defac.firebaseapp.com",
   projectId: "petition-project-defac",
-  storageBucket: "petition-project-defac.appspot.com",
-  messagingSenderId: "553652768003",
-  appId: "1:553652768003:web:a4e701895065dba3ebed36",
-  databaseURL: "https://petition-project-defac-default-rtdb.firebaseio.com",
-  measurementId: "G-YRKTVC6WVF"
 };
 
-firebase.initializeApp(config);
+const app = firebase.initializeApp(config);
+const db = app.firestore();
 
 // Reference messages collection
-var messagesRef = firebase.database().ref('messages');
-var countRef = firebase.database().ref('count');
+const messagesRef = db.collection("messages");
+const countRef = db.collection("count");
 
 // Listen for form submit
 document.getElementById('contactForm').addEventListener('submit', submitForm);
 
 // Attach an asynchronous callback to read the data at our posts reference
-countRef.on('value', (snapshot) => {
-  document.querySelector('.count').innerHTML = snapshot.val();
-}, (errorObject) => {
-  console.log('The read failed: ' + errorObject.name);
-}); 
 
-document.querySelector('.count').innerHTML = (current_value || 0)
+countRef.doc("count")
+  .onSnapshot((doc) => {
+    console.log("Current data: ", doc.data().count);
+    document.querySelector('.count').innerHTML = doc.data()?.count || 0;
+  });
+
+
 // Submit form
-function submitForm(e){
+function submitForm(e) {
   e.preventDefault();
 
   //Get value
-  var name = getInputVal('name');
-  var company = getInputVal('company');
-  var email = getInputVal('email');
-  var phone = getInputVal('phone');
-  var message = getInputVal('message');
+  const name = getInputVal('name');
+  const company = getInputVal('company');
+  const email = getInputVal('email');
+  const phone = getInputVal('phone');
+  const message = getInputVal('message');
 
   // Save message
   saveMessage(name, company, email, phone, message);
@@ -44,23 +41,22 @@ function submitForm(e){
   // Show alert
   document.querySelector('.alert').style.display = 'block';
   // // Hide alert after 3 seconds
-  setTimeout(function(){
+  setTimeout(function () {
     document.querySelector('.alert').style.display = 'none';
-  },3000);
+  }, 3000);
 
   // Clear form
   document.getElementById('contactForm').reset();
 }
 
 // Function to get form value
-function getInputVal(id){
+function getInputVal(id) {
   return document.getElementById(id).value;
 }
 
 // Save message to firebase
-function saveMessage(name, company, email, phone, message){
-  var newMessageRef = messagesRef.push();
-  newMessageRef.set({
+function saveMessage(name, company, email, phone, message) {
+  messagesRef.add({
     name: name,
     company: company,
     email: email,
@@ -68,9 +64,8 @@ function saveMessage(name, company, email, phone, message){
     message: message
   });
 
-  countRef.transaction(function (current_value) {
-    return (current_value || 0) + 1;
+  countRef.doc("count").update({
+    count: firebase.firestore.FieldValue.increment(1)
   });
 
 }
-
